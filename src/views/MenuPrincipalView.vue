8ad67305b511b4182df11d6d554e954cf9c4ff07
Matheus <matheushagedorn@univille.br>
2024-04-24 19:04:50 -0300

    modal

--- src/views/MenuPrincipalView.vue
+++ src/views/MenuPrincipalView.vue
@@ -3,7 +3,7 @@
     <div class="menu-principal-form">
       <h1>Desafio MemórIA</h1>
       <img src="@/assets/img/brain.png" />
-      <button @click="openModal">Jogar</button>
+      <button @click="openGameModal">Jogar</button>
       <router-link to="como-jogar"><button>Como jogar</button></router-link>
       <router-link to="creditos"><button>Créditos</button></router-link>
     </div>
@@ -11,5 +11,12 @@
 </template>
 
 <script setup>
+import { useModal } from '@kolirt/vue-modal';
+
+const { openModal, closeModal } = useModal();
+
+const openGameModal = () => {
+  openModal('game-modal');
+};
 
 </script>
