b8362fa0c11c3abd4a91610fc0b83aed6a9c49dc
Matheus Hagedorn <matheus.hagedorn@gmail.com>
2024-04-29 14:17:05 -0300

    ideia inicial da lógica do jogo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createModal } from '@kolirt/vue-modal'
 import App from './App.vue'
 import router from './router'
 
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,9 +1,35 @@
 <template>
-    <div class="game">
-        <div class="game-container">
-            <TituloPagina titulo="Jogar" link="/menu-principal" />
-        </div>
+  <div class="game">
+    <div class="game-container">
+      <TituloPagina titulo="Jogar" link="/menu-principal" />
+      <div class="game-container-cards">
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+          <div class="game-container-cards-item"><img src="@/assets/img/card.png"></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
