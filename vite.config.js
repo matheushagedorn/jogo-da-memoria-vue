c167cb5dccd775e186b87a497a71c61d73d4f64e
Matheus <matheushagedorn@univille.br>
2024-05-13 12:51:21 -0300

    deploy configurado v6

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/menu-principal",
+  base: "/jokenpo-ia-vue",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
